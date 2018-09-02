ca70dc5882355025bf965e5786f13858814ddd2c
Bruno Martins <bgcngm@gmail.com>
2018-09-03 00:40:59 +0200

    potter: Adapt for local LineageHW java overlays
    Change-Id: Icb2b9810217f4b4d442ab83a16f455bd2ed82071

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,8 +179,7 @@ TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Lineage Hardware
-BOARD_HARDWARE_CLASS += \
-    $(DEVICE_PATH)/lineagehw
+JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
 
 # Manifest
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifest.xml
