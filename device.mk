bcce022479584a003290555bad25436a19382637
Vachounet <vachounet@live.fr>
2019-01-23 16:16:30 +0100

    potter: switch to binderized vibrator service
    Change-Id: I24cc8998aa0b38e090fd40515795bb4f313b081b

--- device.mk
+++ device.mk
@@ -398,7 +398,7 @@ PRODUCT_PACKAGES += \
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service.potter
 
 PRODUCT_PACKAGES += \
     vndk-sp
