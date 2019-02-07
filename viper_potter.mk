7d698b0c89fd4168b8317af352d4ee8829e50c2e
sandyorton7 <sandyorton7@gmail.com>
2019-02-07 17:52:14 +0000

    add moi name

--- viper_potter.mk
+++ viper_potter.mk
@@ -45,3 +45,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # for specific
 $(call inherit-product, vendor/motorola/potter/potter-vendor.mk)
+
+#Le Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.viper.maintainer=thenewguyhere(Sri Sandeep)
